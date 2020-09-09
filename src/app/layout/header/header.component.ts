8ffc8f4fe065309c0e54bd2b8de5e07d8c2f4ca6
olekgalgan <olekgalgan@gmail.com>
2020-09-09 20:46:12 +0200

    Add mobile menu

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -7,11 +7,35 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  mobileMenuSelected: boolean = false;
+  @ViewChild('menu', { static: true }) menu: ElementRef;
   constructor(private translateService: TranslateService) {}
 
   ngOnInit(): void {}
 
   onSwitchLanguage(language: string) {
     this.translateService.use(language);
+  }
+
+  onToggleMenu() {
+    if (this.mobileMenuSelected) {
+      this.menu.nativeElement.style.display = 'none';
+      this.menu.nativeElement.style.transform = 'translateX(100%)';
+      document.querySelector('body').style.overflowY = 'visible';
+    } else {
+      this.menu.nativeElement.style.display = 'flex';
+      this.menu.nativeElement.style.transform = 'translateX(0)';
+      document.querySelector('body').style.overflowY = 'hidden';
+    }
+    this.mobileMenuSelected = !this.mobileMenuSelected;
+  }
+
+  onHideMenu() {
+    if (this.mobileMenuSelected) {
+      this.mobileMenuSelected = !this.mobileMenuSelected;
+      this.menu.nativeElement.style.display = 'none';
+      this.menu.nativeElement.style.transform = 'translateX(100%)';
+      document.querySelector('body').style.overflowY = 'visible';
+    }
   }
 }
